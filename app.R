23a7d1758c1aeae8c6c1eaa586cc96e0c691a5f6
simonayala <simonlisandroayala@gmail.com>
2023-05-03 20:53:46 -0300

    Agregamos footer

--- app.R
+++ app.R
@@ -1,20 +1,86 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 
-ui <- fluidPage(
-  titlePanel("Simon apps")
+# Define UI
+ui <- fluidPage(  
+  includeCSS("assets/bootstrap.min.css"),
+  tags$head(
+    tags$link(rel = "stylesheet", type = "text/css", href = "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css")
+  ),
+  # Header
+  headerPanel("My App"),
+  
+  # Sidebar
+  sidebarLayout(
+    sidebarPanel(
+      # Sidebar content
+    ),
+    
+    # Main panel
+    mainPanel(
+      # Main panel content
+    )
+  ),
+
+  tags$br(),
+  tags$br(),
+  tags$br(),
+  tags$br(),
+  tags$br(),
+  tags$br(),
+  tags$br(),
+  tags$br(),
+  tags$br(),
+  tags$br(),
+  tags$br(),
+  tags$br(),
+  tags$br(),
+  tags$br(),
+  tags$br(),
+  tags$br(),
+  tags$br(),
+  
+  tags$footer(
+    # Footer
+    tags$div(
+      tags$div(
+        tags$div(
+          tags$div(
+            tags$a(
+              href = "https://twitter.com/fundacionDER",
+              target = "_blank", class="btn mx-3 btn-lg btn-floating",
+                   tags$i(class="bi bi-twitter")
+            ),
+            tags$a(
+              href = "https://www.instagram.com/democraciaenred/",
+              target = "_blank",
+              class="btn mx-3 btn-lg btn-floating",
+                   tags$i(class="bi bi-instagram")
+            ),
+            tags$a(href = "https://www.facebook.com/democraciaenred", target = "_blank", class="btn mx-3 btn-lg btn-floating",
+                   tags$i(class="bi bi-facebook")
+            ),
+            tags$a(href = "https://www.linkedin.com/company/democracia-en-red/", target = "_blank", class="btn mx-3 btn-lg btn-floating",
+                   tags$i(class="bi bi-linkedin")
+            ),
+            class="text-center"  ),
+                 class="col"),
+        class="row"),
+      class="container")
+,
+    class="text-white position-relative",
+    id="footer"
+    
+  )
+
 )
 
+
+
+
+# Define server
 server <- function(input, output) {
+  # Server logic
 }
 
-# Run the application 
-shinyApp(ui = ui, server = server)
+# Run the app
+shinyApp(ui, server)
